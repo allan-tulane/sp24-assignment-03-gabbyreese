d2a010fb4373e9211d2a8ee3277a94773108b700
gabbyreese <157334156+gabbyreese@users.noreply.github.com>
2024-05-09 01:12:49 +0000

    Done.

--- answers.md
+++ answers.md
@@ -1,8 +1,14 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:** Gabby Reese
 
+1a) The greedy algorithm you could use is to take as many coins of the largest denomination possible first. You lower to the next demoniation after you can't use a denomination. 
+1b)
+1c) The algorithm and span is O(logN).
 
-Place all written answers from `assignment-03.md` here for easier grading.
+2a) One counterexample is assume Fortuito has denominations [10, 6, 1]. If you go to Fortuito and need change for 12, the greedy algorithm designates that you receive 3 coins: 10, 1, and 1. However, the optimal solution in this case would be to ignore the largest value and take 2 coins of value 6.
+2b) The optimal substructure property means that the optimal solution to a given problem can be divided into the optimal solution to sub-problems of the original problem.
+2c) d is an array of denomination values, n is the amount of change to be made.
+
 
--- main.py
+++ main.py
@@ -16,14 +16,58 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:])))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+      m = len(S)
+      n = len(T)
+     
+      fMED = [[0 for x in range(n + 1)] for x in range(m + 1)]
+      for i in range(m + 1):
+        for j in range(n + 1):
+          if i == 0:
+            fMED[i][j] = j # Min. operations = j
+          elif j == 0:
+            fMED[i][j] = i
+          elif S[i-1] == T[j-1]:
+            fMED[i][j] = fMED[i-1][j-1]
+          else:
+            fMED[i][j] = 1 + min(fMED[i][j-1], fMED[i-1][j], fMED[i-1][j-1])
+      return fMED
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    fMED = fast_MED(S, T)
+    S_align = []
+    T_align = []
+    i = len(S)
+    j = len(T)
+    while True:
+      if(i == 0 and j==0):
+        break
+      else:
+        insert = fMED[i][j-1]
+        remove = fMED[i-1][j]
+        sub = fMED[i-1][j-1]
+        minimum = min(insert,remove,sub)
+        if(sub == minimum):
+          S_align = [S[i-1]] + S_align
+          T_align = [T[j-1]] + T_align
+          i = i-1
+          j = j-1
+        elif(insert == minimum):
+          S_align = ['-'] + S_align
+          T_align = [T[j-1]] + T_align
+          j = j-1
+        elif(remove == minimum):
+          T_align = ['-'] + T_align
+          S_align = [S[i-1]] + S_align
+          i = i-1
+
+    s_str = ""
+    t_str = ""
+    s_str = s_str.join(S_align)
+    t_str = t_str.join(T_align)
+
+    return s_str, t_str
 
